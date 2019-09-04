60bcf99581a90d83ec2ab614238af99c976332c9
Trevor Campbell <trevor.d.campbell@gmail.com>
2019-09-03 17:51:48 -0700

    added day of week colmn to flight delay data

--- raw_data/flight-delays/clean_data.py
+++ raw_data/flight-delays/clean_data.py
@@ -3,7 +3,7 @@ import pandas as pd
 table = pd.read_csv('flights.csv')
 
 bos_departures = table[ table['ORIGIN_AIRPORT'] == 'BOS' ]
-bos_deps_subset = bos_departures[['YEAR', 'MONTH', 'DAY', 'DESTINATION_AIRPORT', 'DISTANCE', 'SCHEDULED_DEPARTURE', 'DEPARTURE_DELAY', 'SCHEDULED_ARRIVAL', 'ARRIVAL_DELAY', 'DIVERTED', 'CANCELLED']]
+bos_deps_subset = bos_departures[['YEAR', 'MONTH', 'DAY', 'DAY_OF_WEEK', 'DESTINATION_AIRPORT', 'DISTANCE', 'SCHEDULED_DEPARTURE', 'DEPARTURE_DELAY', 'SCHEDULED_ARRIVAL', 'ARRIVAL_DELAY', 'DIVERTED', 'CANCELLED']]
 
 print(bos_deps_subset.head())
 
