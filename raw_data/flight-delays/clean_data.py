93f16728cb4d1b38caf3762aca987972940cbbaa
Trevor Campbell <trevor.d.campbell@gmail.com>
2019-09-03 18:01:12 -0700

    added dep/arr, sql output

--- raw_data/flight-delays/clean_data.py
+++ raw_data/flight-delays/clean_data.py
@@ -1,13 +1,24 @@
 import pandas as pd
+from sqlalchemy import create_engine
 
+#create the SQL engine to output sqlite database
+engine = create_engine('sqlite://', echo=False)
+
+#load raw flight data
 table = pd.read_csv('flights.csv')
 
-bos_departures = table[ table['ORIGIN_AIRPORT'] == 'BOS' ]
-bos_deps_subset = bos_departures[['YEAR', 'MONTH', 'DAY', 'DAY_OF_WEEK', 'DESTINATION_AIRPORT', 'DISTANCE', 'SCHEDULED_DEPARTURE', 'DEPARTURE_DELAY', 'SCHEDULED_ARRIVAL', 'ARRIVAL_DELAY', 'DIVERTED', 'CANCELLED']]
+#take subset 
+bos_flights = table[ (table['ORIGIN_AIRPORT'] == 'BOS') | (table['DESTINATION_AIRPORT'] == 'BOS')]
+bos_fls_subset = bos_flights[['YEAR', 'MONTH', 'DAY', 'DAY_OF_WEEK', 'ORIGIN_AIRPORT', 'DESTINATION_AIRPORT', 'DISTANCE', 'SCHEDULED_DEPARTURE', 'DEPARTURE_DELAY', 'SCHEDULED_ARRIVAL', 'ARRIVAL_DELAY', 'DIVERTED', 'CANCELLED']]
 
-print(bos_deps_subset.head())
+#print head to check
+print(bos_fls_subset.head())
 
-bos_deps_subset.to_csv('flights_filtered.csv', index=False)
+#output to csv; don't output row numbers
+bos_fls_subset.to_csv('flights_filtered.csv', index=False)
+
+#output to SQLite; don't output row numbers
+bos_fls_subset.to_sql('flights_filtered.sqlite', con=engine, index=False)
 
 
 
