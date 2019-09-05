c381ebe0965e8ceba202c92fc7d787b814b4df52
Trevor Campbell <trevor.d.campbell@gmail.com>
2019-09-05 07:17:21 -0700

    last update to flight-delays before wksht2 done

--- raw_data/flight-delays/clean_data.py
+++ raw_data/flight-delays/clean_data.py
@@ -9,20 +9,25 @@ engine = create_engine('sqlite:///flights_filtered.db', echo=False)
 #load raw flight data
 table = pd.read_csv('flights.csv')
 
+
+
 #take subset 
 bos_flights = table[ (table['ORIGIN_AIRPORT'] == 'BOS') | (table['DESTINATION_AIRPORT'] == 'BOS')]
-bos_fls_subset = bos_flights[['YEAR', 'MONTH', 'DAY', 'DAY_OF_WEEK', 'ORIGIN_AIRPORT', 'DESTINATION_AIRPORT', 'DISTANCE', 'SCHEDULED_DEPARTURE', 'DEPARTURE_DELAY', 'SCHEDULED_ARRIVAL', 'ARRIVAL_DELAY', 'DIVERTED', 'CANCELLED']]
+bos_flights = bos_flights[['YEAR', 'MONTH', 'DAY', 'DAY_OF_WEEK', 'ORIGIN_AIRPORT', 'DESTINATION_AIRPORT', 'DISTANCE', 'SCHEDULED_DEPARTURE', 'DEPARTURE_DELAY', 'SCHEDULED_ARRIVAL', 'ARRIVAL_DELAY', 'DIVERTED', 'CANCELLED']]
 
-bos_fls_subset['DAY_OF_WEEK'] = bos_fls_subset['DAY_OF_WEEK'] +  (np.floor(bos_fls_subset['SCHEDULED_DEPARTURE'] / 100)*60 + (bos_fls_subset['SCHEDULED_DEPARTURE'] % 100))/(24*60)
+#remove NA rows
+bos_flights = bos_flights.dropna()
+
+#bos_fls_subset['DAY_OF_WEEK'] = bos_fls_subset['DAY_OF_WEEK'] +  (np.floor(bos_fls_subset['SCHEDULED_DEPARTURE'] / 100)*60 + (bos_fls_subset['SCHEDULED_DEPARTURE'] % 100))/(24*60)
 
 #print head to check
-print(bos_fls_subset.head())
+print(bos_flights.head())
 
 #output to csv; don't output row numbers
-bos_fls_subset.to_csv('flights_filtered.csv', index=False)
+bos_flights.to_csv('flights_filtered.csv', index=False)
 
 #output to SQLite; don't output row numbers
-bos_fls_subset.to_sql('bos_flights', con=engine, index=False)
+bos_flights.to_sql('bos_flights', con=engine, index=False)
 
 
 
